5a4bb8a8769e2c0a1752bb729c1438c4c53ea95c
Radhakrishna Venkatesha, Pradeep <pradeep.radhakrishna.venkatesha@intel.com>
2022-01-15 22:41:20 +0530

    na

--- strings/reverse.py
+++ strings/reverse.py
@@ -1,10 +1,17 @@
 # Reverse a string 
   
+from xml.sax.xmlreader import InputSource
+
+
 inputString = "Hello World"
 def reverse(str):
   tmpString = ""
   for i in str:
     tmpString = i + tmpString
   return tmpString
-  
+
+print('Input String: ', inputString)
+print('Input String Length: ', len(inputString))
+for i in range(len(inputString)):
+    print(inputString[i])
 print ("The reversed string is : ", reverse(inputString))
